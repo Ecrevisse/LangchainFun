c75a2d17381c8a3a925cf6dc21fa18c055ba0079
romeo <romeo@selas.studio>
2023-10-24 10:46:46 +0200

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@ mkdir models
 cd models
 wget https://huggingface.co/TheBloke/Llama-2-13B-chat-GGUF/resolve/main/llama-2-13b-chat.Q5_K_M.gguf
 cd ..
+pip install -U pip
 pip install -r requirements.txt
